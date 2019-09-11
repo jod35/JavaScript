af80535485043c215e4d4ea84b1561b89680f9ea
jod35 <jodestrevin@gmail.com>
2019-09-11 10:18:14 -0400

    commit 2

---
+++ strings.js
@@ -0,0 +1,28 @@
+//strings are basically words
+
+//e.g
+// var Sister ="Pretty";
+// var Brother ="Jeremiah";
+
+// //strings are concatenated using the + operator
+// //e.g
+
+// console.log("My brother is " + " " + Brother + ".");
+// console.log("My sister is " + " " + Sister + ".");
+
+// //My brother is  Jeremiah.
+// //My sister is  Pretty.
+
+//finding the length of a string
+
+// var myName ="Jonathan";
+// console.log(myName.length);
+
+//returns 8
+
+//bracket notation helps us to find characters wiwthin a string
+// var myvar = "Variable";
+
+// console.log(myvar[3]);
+
+// //returns 'i'
