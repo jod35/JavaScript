22e3a0374c71f920ead28f0dab9bcc4b207ca354
jod35 <jodestrevin@gmail.com>
2019-09-12 11:57:39 -0400

    commit 2

---
+++ arrays.js
@@ -0,0 +1,31 @@
+//Arrays helps us to store multiple values.
+
+// var myArray=[];
+// //elements can be of any data type
+
+// var any= ["Jona",1,2,"##"];
+
+// nested array /multidimesnional arrays
+// arrays containig other arrays
+// var arrays= [["Jon","Jessica"],[1,2,3,4,3],[{"name":"jonathan"},{"name":"Jessica"}]];
+
+//accessing certain elements of an array
+var ourArray =['Jonathan',['Leti','Ogwal'],1,3,4];
+
+var mydata = ourArray[0];
+
+console.log(mydata);
+
+//returns Jonathan
+
+//modifying an array
+
+ourArray[0] = "Pretty";
+console.log(ourArray);
+// returns [ 'Pretty', [ 'Leti', 'Ogwal' ], 1, 3, 4 ]
+//replaces Jonathan with Pretty
+
+
+
+
+
---
+++ functions.js
@@ -0,0 +1,34 @@
+//functions help us to have reuseable code
+
+// function OurFunction(){
+//     console.log("Hello World!!");
+// }
+
+// OurFunction(); 
+
+//functions with args
+
+// function AddNums(a,b){
+//     return a+b;
+// }
+// console.log(AddNums(2,3));
+// //returns 5
+
+//A function that compares two numbers
+
+// function CompareNums(a,b){
+//     if(a<b){
+//         return a + " " + " is less than " + " " + b
+//     }
+//     else{
+//         return a + " " + "is greater than " + " " + b
+//     }
+// }
+
+// console.log(CompareNums(100,59));
+// console.log(CompareNums(23,100));
+
+// returns 
+// 100 is greater than  59
+// 23  is less than  100
+
